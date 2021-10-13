0b8f1848143779083668844f78502a63130f24e7
lorraine98 <hyosung0425@naver.com>
2021-10-14 07:43:37 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    extensions: [".vue", ".js"],
+    alias: {
+      "~": path.resolve(__dirname, "src"),
+    },
+  },
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    clean: true,
+    //생략시 기본 이름으로 설정됨.
+    //filenmae:'hello.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          "vue-style-loader",
+          "css-loader",
+          "postcss-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              additionalData: `
+                @use "sass:color";
+                @use "sass:list";
+                @use "sass:map";
+                @use "sass:math";
+                @use "sass:meta";
+                @use "sass:selector";
+                @use "sass:string";
+                @import "~/scss/_variables";
+              `,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlPlugin({
+      template: "src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "static" }, //to 옵션은 output의 path로 기본값을 가져 생략 가능.
+      ],
+    }),
+  ],
+  //실제 배포는 호스팅 서버를 작성해야 함.
+  devServer: {
+    historyApiFallback: true,
+  },
+};
